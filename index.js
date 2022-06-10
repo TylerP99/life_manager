f2d733693d1dd92acfe93324bc234027eeb32020
Tyler Perata <tylerperata@gmail.com>
2022-06-10 15:05:06 -0700

    Add tasks ejs, start of server, gitignore modification

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect()
 
 const app = express();
+app.set("view engine","ejs");
 
-const PORT = 8000;
+const dbName = "idk";
+const dbConnectionString = process.env.DB_STRING;
 
-app.get("/", (req,res) => {
-    res.sendFile(__dirname + "/index.html");
+
+app.get("/tasks", (req, res) => {
+    //Get task data from db
+
+    //Format for ejs
+
+    //Render ejs
 });
 
-app.listen(PORT, () => {
-    console.log("Listening on port " + PORT);
+app.post("/create/task", (req, res) => {
+    //Validate info
+
+    //Format for db
+
+    //Add to db (user func comes next)
 });
--- public/js/tasks.js
+++ public/js/tasks.js
@@ -1,11 +1,17 @@
-
-
 class Task {
-    constructor(name, description, start, end)
+    constructor(name, description, start, end, color)
     {
-        this.name = name;
-        this.description = description;
-        this.startTime = start;
-        this.endTime = end;
+        this.name = name; // String, idk char limit yet. 50 seems reasonable
+        this.description = description; // String (text field for db semantics) (limit unclear right now)
+        this.startTime = start;  // Should be a date object
+        this.endTime = end; // Should be a date object
+        this.color = color; // Should be a hex code probably
     }
+}
+
+// Need to interface this with calendar somehow later. Actually calendar interfacing should be generic if this app expands
+
+
+class Task_Display {
+    
 }
