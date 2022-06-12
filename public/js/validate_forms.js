0867e67c60bced069de2c78dbe776c88e043a817
Tyler Perata <tylerperata@gmail.com>
2022-06-11 21:38:09 -0700

    Basic client side registration validation started

--- public/js/validate_forms.js
+++ public/js/validate_forms.js
@@ -23,6 +23,11 @@ emailField.addEventListener("change", validateEmail);
 function validateEmail(event) {
     //Follows correct format
     //Not in db already (interest problem to solve async) (need to keep confirm button off until this is resolved)
+    const regStr = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/);
+    const email = event.target.value;
+
+    let valid = regStr.test(email);
+    console.log(valid);
 }
 
 //password HANDLER
@@ -30,10 +35,15 @@ passwordField.addEventListener("change", validatePassword);
 
 function validatePassword(event) {
     //8 chars at least
+    const regStr = new RegExp(/.{8,}/)
+    const password = event.target.value;
+
+    let valid = regStr.test(password);
+    console.log(valid)
 }
 
 //USERNAME HANDLER
-usernameField.addEventListener("change", validateUsername);
+usernameField.addEventListener("change", validatePasswordConf);
 
 function validatePasswordConf(event) {
     //Should match password
