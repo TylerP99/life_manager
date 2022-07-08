3cd11b4f773eb812bf5ae53f9013522fcbd7c783
Tyler Perata <tylerperata@gmail.com>
2022-07-07 19:04:31 -0700

    User registration add

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 
+const PORT = 3010;
+
 // 
 // Middle-wares and such
 //
@@ -11,13 +13,18 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
+//Bcrypt
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 //
 // MongoDB connection
 //
-const dbName = "tasks";
-const dbConnectionString = process.env.DB_STRING;
+const dbName = "LifeManager"; // Connect to DB
+const dbConnectionString = process.env.DB_STRING; // Connect to Cluster
+const userCollection = "LM-users";
+const tasksCollection = "LM-tasks";
 let db;
-const PORT = 3010;
 
 MongoClient.connect(dbConnectionString, {useUnifiedTopology: true})
            .then(client => {
@@ -52,7 +59,7 @@ app.post("/create/task", (req, res) => {
     const task = new Task(reqName, reqDesc, reqStart, reqEnd, reqColor);
 
     //Add to db (user func comes next)
-    db.collection("task-list")     //Get collection
+    db.collection(tasksCollection)     //Get collection
       .insertOne(task)  //Insert task object
       .then( result => {
           console.log("Added task");
@@ -73,7 +80,7 @@ app.post("/create/task", (req, res) => {
 //
 // SIGN UP FORM HANDLER (Create Account)
 //
-app.post("/user/signup", (req, res) => {
+app.post("/user/signup", async (req, res) => {
     // Get form data from request
     const userData = {
         username:req.body.username,
@@ -82,24 +89,102 @@ app.post("/user/signup", (req, res) => {
         passwordConfirmation:req.body.passwordConf,
     }
 
+    const errorData = {
+        usernameErrors:{
+            empty:false,
+        },
+        emailErrors:{
+            empty:false,
+            alreadyExists:false,
+        },
+        passwordErrors:{
+            empty:false,
+            short:false,
+            mismatchConf:false,
+        }
+    }
+
+    let valid = true;
+
     // Validate form info
     // Validate name
+    if(!userData.username)
+    {
+        // No name entered
+        errorData.usernameErrors.empty = true;
+        valid = false;
+    }
 
     // Validate email
     // Confirmation email? Best way to validate i guess
     // Ensure email is unique
+    if(!userData.email)
+    {
+        // No email entered
+        errorData.emailErrors.empty = true;
+        valid = false;
+    }
+    const emailExists = await db.collection(userCollection)
+                          .findOne( {"email": userData.email});
+
+    console.log(emailExists);
+    if(emailExists)
+    {
+        console.log("email exists")
+        errorData.emailErrors.alreadyExists = true;
+        valid = false;
+    }
+
 
     // Validate password
     // 8 char minimum
     // PasswordConf matches
+    if(!userData.password)
+    {
+        // No password entered
+        errorData.passwordErrors.empty = true;
+        valid = false;
+    }
+    else if(userData.password.length < 8)
+    {
+        // Password too short
+        errorData.passwordErrors.short = true;
+        valid = false;
+    }
 
-    // Hash password
+    if(userData.password !== userData.passwordConfirmation)
+    {
+        // Passwords dont match
+        errorData.passwordErrors.mismatchConf = true;
+        valid = false;
+    }
 
     // If valid, create new db entry for user
+    if(valid) {
+        // Delete passwordConf from user object
+        delete userData.passwordConfirmation;
 
-    // then provide some feedback that an account was create
+        // Hash password
+        bcrypt.hash(userData.password, saltRounds, (err, hash) => {
+            // Save hashed password in object
+            userData.password = hash;
 
-    // If invalid, provide some feedback for what is invalid
+            // Add user object to database
+            db.collection(userCollection)
+              .insertOne(userData)
+              .then(result => {
+                  console.log("Added user to database")
+                  res.redirect("/html/signin.html")
+              })
+              .catch(err => console.error(err))
+        });
+    }
+    else
+    {
+        // If invalid, provide some feedback for what is invalid
+        console.log("Invalid")
+        res.json(errorData);
+    }
 });
 
 //
