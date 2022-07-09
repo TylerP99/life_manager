2febcb34bde6fdc733bf4a0f4b18416b169e8e05
Tyler Perata <tylerperata@gmail.com>
2022-07-08 19:03:03 -0700

    Add mongoose schemas for user and task

--- models/Task.js
+++ models/Task.js
@@ -1,1 +1,37 @@
 //Mongoose tasks schema
+
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true,
+        default: "New Task"
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    startTime: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    endTime: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    color: {
+        type: String,
+        default: "#000"
+    },
+    creationTime: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Task = mongoose.model("Task", TaskSchema);
+
+module.exports = Task;
