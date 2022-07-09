4b6512dcb88c991203db66a7547b2e40c41b7bd5
Tyler Perata <tylerperata@gmail.com>
2022-07-08 19:41:14 -0700

    Add register helper

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 
-// NEED TO IMPORT USER SCHEMA WHEN CREATED
+// Get user schema
+const User = require("../models/User");
 
 //==============================//
 //      User Registration       //
@@ -24,75 +25,8 @@ router.post("/user/signup", (req,res) => {
         passwordConfirmation:req.body.passwordConf,
     }
 
-    const errorData = {
-        usernameErrors:{
-            empty:false,
-        },
-        emailErrors:{
-            empty:false,
-            alreadyExists:false,
-        },
-        passwordErrors:{
-            empty:false,
-            short:false,
-            mismatchConf:false,
-        }
-    }
-
-    let valid = true;
-
-    // Validate form info
-    // Validate name
-    if(!userData.username)
-    {
-        // No name entered
-        errorData.usernameErrors.empty = true;
-        valid = false;
-    }
-
-    // Validate email
-    // Confirmation email? Best way to validate i guess
-    // Ensure email is unique
-    if(!userData.email)
-    {
-        // No email entered
-        errorData.emailErrors.empty = true;
-        valid = false;
-    }
-    const emailExists = await db.collection(userCollection)
-                          .findOne( {"email": userData.email});
-
-    console.log(emailExists);
-    if(emailExists)
-    {
-        console.log("email exists")
-        errorData.emailErrors.alreadyExists = true;
-        valid = false;
-    }
-
-
-    // Validate password
-    // 8 char minimum
-    // PasswordConf matches
-    if(!userData.password)
-    {
-        // No password entered
-        errorData.passwordErrors.empty = true;
-        valid = false;
-    }
-    else if(userData.password.length < 8)
-    {
-        // Password too short
-        errorData.passwordErrors.short = true;
-        valid = false;
-    }
-
-    if(userData.password !== userData.passwordConfirmation)
-    {
-        // Passwords dont match
-        errorData.passwordErrors.mismatchConf = true;
-        valid = false;
-    }
+    const validation_obj = validate_user_info(userData)
+    let valid = validation_obj.isValid;
 
     // If valid, create new db entry for user
     if(valid) {
@@ -109,7 +43,7 @@ router.post("/user/signup", (req,res) => {
               .insertOne(userData)
               .then(result => {
                   console.log("Added user to database")
-                  res.redirect("/html/signin.html")
+                  res.redirect("/signin")
               })
               .catch(err => console.error(err))
         });
@@ -118,9 +52,78 @@ router.post("/user/signup", (req,res) => {
     {
         // If invalid, provide some feedback for what is invalid
         console.log("Invalid")
-        res.json(errorData);
+        res.json(validation_obj.errorData);
     }
 });
+
+// Helper functions
+
+function validate_user_info(user) {
+    // Will return object with validation variable and other error info
+
+    const validation_obj = {
+        isValid:true,
+        errorData:{
+            usernameErrors:{
+                emptyField:false,
+            },
+            emailErrors:{
+                alreadyExists:false,
+                emptyField:false,
+            },
+            passwordErrors:{
+                emptyField:false,
+                tooShort:false,
+                doesNotMatchPasswordConf:false
+            }
+        }
+    }
+
+    // Check username field
+    if(!user.email)
+    {
+        // Username field empty
+        validation_obj.errorData.usernameErrors.emptyField = true;
+        validation_obj.isValid = false;
+    }
+
+    // Check email field
+    if(!user.email)
+    {   
+        // Email field empty
+        validation_obj.errorData.emailErrors.emptyField = true;
+        validation_obj.isValid = false;
+    }
+    else if(await User.findOne({"email":user.email}))
+    {
+        // Email in use
+        validation_obj.errorData.emailErrors.alreadyExists = true;
+        validation_obj.isValid = false;
+    }
+
+    // Check password fields
+    if(!user.password)
+    {
+        // Password doesnt exist
+        validation_obj.errorData.passwordErrors.emptyField = true;
+        validation_obj.isValid = false;
+    }
+    else if(user.password.length < 8)
+    {
+        // Password too short
+        validation_obj.errorData.passwordErrors.tooShort = true;
+        validation_obj.isValid = false;
+    }
+
+    if(user.password !== user.passwordConf)
+    {
+        // Passwords dont match
+        validation_obj.errorData.passwordErrors.doesNotMatchPasswordConf = true;
+        validation_obj.isValid = false;
+    }
+
+    return validation_obj;
+}
 
 //==============================//
 //        User Sign In          //
