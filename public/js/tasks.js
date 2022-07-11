0f79f8f099d97ab8727a2d2d469fe7f02dec403e
Tyler Perata <tylerperata@gmail.com>
2022-07-10 19:23:14 -0700

    Task page client side js change

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -1,25 +1,42 @@
-class Task {
-    constructor(name, description, start, end, color)
-    {
-        this.name = name; // String, idk char limit yet. 50 seems reasonable
-        this.description = description; // String (text field for db semantics) (limit unclear right now)
-        this.startTime = start;  // Should be a date object
-        this.endTime = end; // Should be a date object
-        this.color = color; // Should be a hex code probably
-    }
-}
-
-// Need to interface this with calendar somehow later. Actually calendar interfacing should be generic if this app expands
-
-
-//Test driver code for basic functional task page
-
-document.querySelector("#add-task").addEventListener("click", openTaskCreation)
+// Opens and closes the Create Task menu
+document.querySelector("#add-task").addEventListener("click", openTaskCreation);
 
 
 function openTaskCreation(event) {
     const taskCreationMenu = document.querySelector("#add-task-menu");
 
-    taskCreationMenu.classList.toggle("hidden");
-    taskCreationMenu.classList.toggle("activated");
+    toggle_hidden(taskCreationMenu);
+}
+
+// Opens and closes the Edit Task menu
+document.querySelector("#task-edit-start-button").addEventListener("click", openTaskRevision);
+
+function openTaskRevision(event) {
+    const taskRevisionMenu = document.querySelector("#task-edit-form-container");
+
+    toggle_hidden(taskRevisionMenu);
+}
+
+// Opens or hides target element
+function toggle_hidden(elem)
+{
+    elem.classList.toggle("active");
+    elem.classList.toggle("hidden");
+}
+
+// PUT Request for edit task
+document.querySelector("#task-edit-submit-button").addEventListener("click", send_edit_task_request)
+
+function send_edit_task_request() {
+    // Get the form info
+
+    // Make PUT request
+}
+
+// Delete Request for delete task
+document.querySelector("#task-delete-button").addEventListener("click", send_delete_task_request);
+
+function send_delete_task_request()
+{
+    
 }
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const { ensureAuthenticated, forwardAuthenticated } = require("../config/auth");
 const router = express.Router();
 
+const Task = require("../models/Task");
+
 // Landing page
 router.get("/", forwardAuthenticated, (req, res) => {
     res.render("index.ejs");
@@ -16,8 +18,13 @@ router.get("/dashboard", ensureAuthenticated, (req, res) => {
 });
 
 //Task page
-router.get("/tasks", ensureAuthenticated, (req, res) => {
-    res.render("tasks.ejs")
+router.get("/tasks", ensureAuthenticated, async (req, res) => {
+    // Get user tasks from db
+    const tasks = await Task.find({userId:req.user.id});
+
+    console.log(tasks);
+    // Render the page
+    res.render("tasks.ejs", {tasks: tasks});
 });
 
 module.exports = router;
