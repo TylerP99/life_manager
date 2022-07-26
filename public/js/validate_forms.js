8bc393abe35525981219f82a1815b1c662f72163
Tyler Perata <tylerperata@gmail.com>
2022-07-26 12:36:06 -0700

    Move signin signup css to legacy

--- public/js/validate_forms.js
+++ public/js/validate_forms.js
@@ -21,13 +21,7 @@ function validateUsername(event) {
 emailField.addEventListener("change", validateEmail);
 
 function validateEmail(event) {
-    //Follows correct format
-    //Not in db already (interest problem to solve async) (need to keep confirm button off until this is resolved)
-    const regStr = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/);
-    const email = event.target.value;
-
-    let valid = regStr.test(email);
-    console.log(valid);
+    // Best way to validate an email is to send a verification email
 }
 
 //password HANDLER
@@ -47,5 +41,4 @@ passwordConfField.addEventListener("change", validatePasswordConf);
 
 function validatePasswordConf(event) {
     //Should match password
-    console.log(event.target);
 }
