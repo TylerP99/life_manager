48f346d125553b31c6f2f47e29e2ec0dcfdb3ef1
Tyler Perata <tylerperata@gmail.com>
2022-07-26 12:53:31 -0700

    Create new signup validation js file

---
+++ public/js/signup.js
@@ -0,0 +1,13 @@
+// Separate file for validating new user info before sending to server (also validated on server side)
+
+console.log("Linked");
+
+// Get all the important for elements
+
+const usernameField = document.querySelector("#usernameField");
+
+const emailField = document.querySelector("");
+
+const passwordField = document.querySelector("");
+
+const passwordConfirmationField = document.querySelector("");
